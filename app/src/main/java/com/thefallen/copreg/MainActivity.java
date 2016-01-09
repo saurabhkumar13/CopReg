cb52be8386aec9bc29c8af70422d51e9a1041f6c
Anil Kumar Meena <anil4514@gmail.com>
2016-01-09 14:38:45 +0530

    Intial commit

--- app/src/main/java/com/thefallen/copreg/MainActivity.java
+++ app/src/main/java/com/thefallen/copreg/MainActivity.java
@@ -1,13 +1,64 @@
 package com.thefallen.copreg;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = (TextView) findViewById(R.id.logo);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        final int height = size.y;
+
+        int secondsDelayed = 3;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+
+
+                // Prepare the View for the animation
+                view.setVisibility(View.VISIBLE);
+                view.setAlpha(1.0f);
+
+                // Start the animation
+                view.animate()
+                        .translationY(view.getHeight())
+                        .alpha(1.0f)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                view.animate()
+                                        .translationY(-height)
+                                        .alpha(0.0f)
+                                        .setListener(new AnimatorListenerAdapter() {
+                                            @Override
+                                            public void onAnimationEnd(Animator animation) {
+                                                super.onAnimationEnd(animation);
+                                                view.setVisibility(View.GONE);
+                                            }
+                                        });
+                            }
+                        });
+
+            }
+        }, secondsDelayed * 1000);
+
+
     }
 }
