a009a82ab2d19629b24101fd3a885207b820a2c1
saurabh <saurabhkoolkumar@gmail.com>
2016-01-10 14:12:06 +0530

    card layout changes

--- app/src/main/java/com/thefallen/copreg/MainActivity.java
+++ app/src/main/java/com/thefallen/copreg/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.FrameLayout;
@@ -13,9 +12,8 @@ import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    GifImageView view;
+    GifImageView gifImageView;
     FrameLayout splashScreen;
-    View Overshoot;
     Context mContext;
 
     @Override
@@ -24,32 +22,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mContext = this;
-        //Gif view
-        view = (GifImageView) findViewById(R.id.logo);
-        splashScreen = (FrameLayout) findViewById(R.id.splashScreen);
-        Overshoot = (View) findViewById(R.id.Overshoot);
 
-        //in millis
+        //Gif gifImageView
+        gifImageView = (GifImageView) findViewById(R.id.logo);
+        splashScreen = (FrameLayout) findViewById(R.id.splashScreen);
+
+        setLayoutParams();
+
+        splashAnimate();
+
+    }
+
+    public void setLayoutParams() {
+        splashScreen.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, (int) (DisplayHelper.getHeight(mContext) * 1.4)));
+        splashScreen.setTranslationY(-DisplayHelper.dpToPx(50, mContext));
+        FrameLayout.LayoutParams gifLayoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        gifLayoutParams.topMargin = DisplayHelper.getHeight(mContext) / 2;
+        gifImageView.setLayoutParams(gifLayoutParams);
+
+    }
+
+    public void splashAnimate() {
         int splashDelay = 2400;
-        final int splashDuration = 1000;
-        final int toolbarHeight = DisplayHelper.getHeight(mContext)/5;
 
         new Handler().postDelayed(new Runnable() {
+            //in millis
+            int splashDuration = 1000;
 
             public void run() {
 
                 // Prepare the View for the animation
-                view.setVisibility(View.VISIBLE);
-                view.setAlpha(1.0f);
-                Log.i("height", toolbarHeight+"");
+                gifImageView.setVisibility(View.VISIBLE);
+                gifImageView.setAlpha(1.0f);
                 // Start the animation
                 //animate down
                 splashScreen.animate()
                         .setDuration(splashDuration)
-                        .translationY(-splashScreen.getHeight() + toolbarHeight)
+                        .translationY(-DisplayHelper.getHeight(mContext))
                         .setInterpolator(new AnticipateOvershootInterpolator(1));
             }
 
         }, splashDelay);
     }
+
 }
