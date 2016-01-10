96b3d682af26ae35ddc9027c55f23a491d46fb75
Anil Kumar Meena <anil4514@gmail.com>
2016-01-10 12:10:00 +0530

    height programatically teamview

--- app/src/main/java/com/thefallen/copreg/MainActivity.java
+++ app/src/main/java/com/thefallen/copreg/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.FrameLayout;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         //in millis
         int splashDelay = 2400;
         final int splashDuration = 1000;
+        final int toolbarHeight = DisplayHelper.getHeight(mContext)/5;
 
         new Handler().postDelayed(new Runnable() {
 
@@ -39,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
                 // Prepare the View for the animation
                 view.setVisibility(View.VISIBLE);
                 view.setAlpha(1.0f);
-
+                Log.i("height", toolbarHeight+"");
                 // Start the animation
                 //animate down
                 splashScreen.animate()
                         .setDuration(splashDuration)
-                        .translationY(-splashScreen.getHeight() + DisplayHelper.dpToPx(200,mContext))
+                        .translationY(-splashScreen.getHeight() + toolbarHeight)
                         .setInterpolator(new AnticipateOvershootInterpolator(1));
             }
 
